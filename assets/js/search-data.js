fb48d55c6cb29ac42b5481ae13f1ca20f810d1bc
ShutianLiuinfinite <150165089+ShutianLiuinfinite@users.noreply.github.com>
2026-04-27 11:24:54 +0000

    Deploying to gh-pages from @ ShutianLiuinfinite/shutianliuinfinite.github.io@63e9cbeacfb92a5a2b49857b9b0282150fa0a7c5 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-research",
           title: "research",
-          description: "Working papers and research in progress. Reverse chronological order.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/publications/";
